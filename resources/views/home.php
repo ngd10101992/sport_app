ee27ce0875a6ebe2d0b6322c3fdd8f7e2b415f90
Gia Dat <ngd10101992@gmail.com>
2021-12-01 09:02:42 +0900

    create sports app and auth

---
+++ resources/views/home.php
@@ -0,0 +1,8 @@
+<div class="form-group">
+    <div class='input-group date' id='datetimepicker'>
+        <input type='text' class="form-control" />
+        <span class="input-group-addon">
+            <span class="glyphicon glyphicon-calendar"></span>
+        </span>
+    </div>
+</div>
